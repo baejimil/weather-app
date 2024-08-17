75ff9757782a3708927acebd7fc2d882b0235833
baejimil <qowhdgus629@naver.com>
2024-08-18 01:03:31 +0900

    practice

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,7 +5,7 @@ const WeatherBox = ({weather}) => {
   return (
     <div className='weather-box'>
         <div>서울</div>
-        <h2>{weather && weather.main.temp} / 230화씨</h2>
+        <h2>30도 / 230화씨</h2>
         <h3>맑은 하늘</h3>
     </div>
   )
